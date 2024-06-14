b2ae23484b9bb78d78cdf80544b5cde57021394a
Joshua Coles <josh@coles.to>
2024-06-14 14:23:02 +0100

    Gem init

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in socket_io.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/socket_io.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "socket_io/version"
+
+module SocketIo
+  class Error < StandardError; end
+  # Your code goes here...
+end
